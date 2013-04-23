e1f47cac4147fdfa29cc26fe480d108634f6f2ca
Dwayne Macgowan <dwaynemac@gmail.com>
2013-04-23 17:00:48 -0300

    Update ssl_support.rb

--- lib/logical_model/ssl_support.rb
+++ lib/logical_model/ssl_support.rb
@@ -2,12 +2,7 @@ class LogicalModel
   module SslSupport
 
     def self.included(base)
-      base.send(:include, InstanceMethods)
       base.send(:extend, ClassMethods)
-    end
-
-    module InstanceMethods
-
     end
 
     module ClassMethods
